0da084ae66557573f32882551eb45825426632ef
Rhodiawithh <149595442+Rhodiawithh@users.noreply.github.com>
2023-11-02 13:40:44 +0100

    Update Projet1.c

--- Projet1.c
+++ Projet1.c
@@ -8,9 +8,7 @@ int choix(int option){
     scanf("%d", &option);
     printf("taper un entier entre 1 et 5:");
     scanf("%d", &option);
-    if (option==1){
-        printf("REGLES DU JEU :\n le joueur possede trois vies. Chaque niveau doit etre resolu en moins de 120 secondes.\nLe but est de recuperer les quatres oiseaux du niveau sans se faire toucher par la balle et/ou les ennemis (si present)");
-    }
+    
     return option;
 }
 
