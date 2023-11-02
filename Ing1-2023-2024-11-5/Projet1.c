e7698bb64822bf81c2961fbffc70fe32f4457d89
Rhodiawithh <149595442+Rhodiawithh@users.noreply.github.com>
2023-11-02 13:29:33 +0100

    Add files via upload

---
+++ Ing1-2023-2024-11-5/Projet1.c
@@ -0,0 +1,31 @@
+//
+// Created by chano on 01/11/2023.
+//
+#include "Projet1.h"
+
+int choix(int option){
+    printf("taper un entier entre 1 et 5:");
+    scanf("%d", &option);
+    printf("taper un entier entre 1 et 5:");
+    scanf("%d", &option);
+
+    return option;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
