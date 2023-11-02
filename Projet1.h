d7bf6873057198259e5f84b6408673630009b7a3
Rhodiawithh <chanourhodia@gmail.com>
2023-11-02 12:22:13 +0100

    first commit

---
+++ Projet1.h
@@ -0,0 +1,22 @@
+//
+// Created by chano on 01/11/2023.
+//
+
+#ifndef ING1_2023_2024_11_5_PROJET1_H
+#define ING1_2023_2024_11_5_PROJET1_H
+
+#include <stdio.h>
+
+int choi(int option);
+
+
+
+
+
+
+
+
+
+
+
+#endif //ING1_2023_2024_11_5_PROJET1_H
---
+++ main.c
@@ -0,0 +1,12 @@
+#include "Projet1.h"
+
+int main() {
+    printf("MENU:\n");
+    printf("1.Regles du jeu\n");
+    printf("2.Lancer un nouveau Jeu a partir du niveau 1\n");
+    printf("3.Charger une partie\n");
+    printf("4.Mot de passe\n");
+    printf("5.Scores\n");
+    printf("6.Quitter\n");
+    return 0;
+}
