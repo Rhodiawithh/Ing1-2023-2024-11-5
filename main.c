69006a0bd53cb54f1a63ab9e61221119f43ac9e1
Rhodiawithh <149595442+Rhodiawithh@users.noreply.github.com>
2023-11-02 13:38:31 +0100

    Update main.c

--- main.c
+++ main.c
@@ -10,7 +10,11 @@ int main() {
     printf("6.Quitter\n");
 
     int CHOIX,a;
-    CHOIX = choix(a);
+    a=0;
+    CHOIX= choix(a);
+    if (CHOIX==1){
+        printf("REGLES DU JEU :\n le joueur possede trois vies. Chaque niveau doit etre resolu en moins de 120 secondes.\nLe but est de recuperer les quatres oiseaux du niveau sans se faire toucher par la balle et/ou les ennemis (si present)");
+    }
 
     return 0;
 }
