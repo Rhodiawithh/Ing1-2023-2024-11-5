8306e04aeb6312bf33a396a95bad11c91671ffdd
moruwe <149596496+moruwe@users.noreply.github.com>
2023-11-10 13:55:09 +0100

    Add files via upload

---
+++ SP.h
@@ -0,0 +1,12 @@
+//
+// Created by loris on 10/11/2023.
+//
+
+#ifndef TETS_SP_H
+#define TETS_SP_H
+#include <stdio.h>
+#include <unistd.h>
+void CHRONOMETRE();
+void SauvegardeTemps(int temps);
+void SCORES();
+#endif //TETS_SP_H
